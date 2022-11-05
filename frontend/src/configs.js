e01f3e08599b48911c33c4312ed7ea7a964781ce
johan9se <johannine.enerio@gmail.com>
2022-11-05 22:55:39 +0800

    Update docker files for frontend

--- frontend/src/configs.js
+++ frontend/src/configs.js
@@ -1,5 +1,6 @@
-const URI_BBT_API = process.env.URI_BBT_API || 'http://localhost:8000';
+const NODE_ENV = 'production';
 
+const URI_BBT_API = NODE_ENV === 'production' ? 'http://bbtdiary-env.eba-ea8tzmrw.ap-southeast-1.elasticbeanstalk.com' : 'localhost:8000';
 const PREFIX_BBT_API = '/api/bbts/';
 
 export const URL_BBT_API = URI_BBT_API + PREFIX_BBT_API;
